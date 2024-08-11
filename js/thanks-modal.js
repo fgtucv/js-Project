c09975ae35fc5e5099b75032f070434a737c95c4
fgtucv <oles.slava23@gmail.com>
2024-08-11 16:01:19 +0300

    g

--- js/thanks-modal.js
+++ js/thanks-modal.js
@@ -4,7 +4,23 @@ const backdrop = document.querySelector(".backdrop2");
 const input = document.querySelector(".footer_input");
 const form = document.querySelector(".footer_form");
 
+if(JSON.parse(localStorage.getItem("text")) !== null || JSON.parse(localStorage.getItem("text")) !== ""){
+    input.value = JSON.parse(localStorage.getItem("text"));
+
+    localStorage.removeItem("text");
+}
+
 form.addEventListener("submit", opened);
+
+input.addEventListener("input", sendTextToLocalStorage)
+
+function sendTextToLocalStorage() {
+    const text = input.value;
+
+    localStorage.setItem("text", JSON.stringify(text));
+}
+
+
 
 function opened(event) {
     event.preventDefault();
@@ -13,6 +29,9 @@ function opened(event) {
         backdrop.style.display = "block";
         backdrop.style.opacity = "1";
     }
+
+    localStorage.removeItem("text");
+    input.value = "";
 }
 
 closeButton.addEventListener("click", closed);
