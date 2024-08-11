da645065ff50d8eaffe52a6e6ffb0b9ac90f515d
fgtucv <oles.slava23@gmail.com>
2024-08-11 15:34:51 +0300

    g

--- js/start-modal.js
+++ js/start-modal.js
@@ -1,15 +1,31 @@
+// 1.Перевірити імя в Input
+// 2.відправити значення з Input в localStorage
+// 3.Після перезавантаження сторінки перевірети ч є значення в localStorage
+
 const button = document.querySelector(".start-model_button");
 const backdrop = document.querySelector(".backdrop");
 const input = document.querySelector(".start-model_input");
 const name = document.querySelector(".header_login");
 
-button.addEventListener("click", closeModal);
+console.log(localStorage.getItem("permanentUserName"))
 
-function closeModal() {
-    if (input.value !== "" || undefined) {
-        backdrop.style.display = "none";
 
-        name.textContent = `Вітаю, ${input.value}!`;
+if(localStorage.getItem("permanentUserName") === "" || localStorage.getItem("permanentUserName") === null){
+    backdrop.style.display = "block";
+
+    button.addEventListener("click", closeModal);
+
+    function closeModal() {
+        const userName = input.value;
+    
+        if (userName !== "" || userName !== undefined) {
+                backdrop.style.display = "none";
+    
+                name.textContent = `Вітаю, ${input.value}!`;
+        }
+    
+        localStorage.setItem("permanentUserName", userName);
     }
+} else{
+    name.textContent = `Вітаю, ${localStorage.getItem("permanentUserName")}!`;
 }
-                
